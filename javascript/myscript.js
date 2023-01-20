8cca4d80f1e73562a11705c1234a520ebe6ff538
Rocca-Francesco <roccafrance98@yahoo.it>
2023-01-20 14:55:48 +0100

    scrivo su html da js

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,1 +1,16 @@
-document.writeln("hello world")
+// qual'è il tuo nome?
+let your_name = prompt("Qual'è il tuo nome?");
+
+// qual'è il tuo cognome?
+let your_surname = prompt("Qual'è il tuo cognome?");
+
+// qual'è il tuo colore preferito?
+let favorite_color = prompt("Qual'è il tuo colore preferito?");
+
+// scrivo in html il mio nome e cognome
+document.getElementById("welcome_msg").innerHTML = "Benvenuto " + your_name + " " + your_surname + "!";
+
+// scrivo in html il mio colore preferito
+document.getElementById("best_color").innerHTML = "Il tuo colore preferito é " + favorite_color + " fantastico!";
+
+console.log(favorite_color);
